f0598b942770ca98bc68f0ac141957d174a1414e
John Blanch <39880884+Aushuna@users.noreply.github.com>
2026-01-19 09:23:42 -0500

    Updated the Sea lantern colors
    Updated
    - black
    - blue
    - brown
    - cyan
    - gray
    - green
    - lime
    - magenta
    - orange
    - pink
    - purple
    - red
    - white
    - yellow

--- src/main/java/com/yhord/lightly/colored/sea/lanterns/LightlyColoredSeaLanterns.java
+++ src/main/java/com/yhord/lightly/colored/sea/lanterns/LightlyColoredSeaLanterns.java
@@ -1,42 +1,59 @@
 package com.yhord.lightly.colored.sea.lanterns;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class LightlyColoredSeaLanterns implements ModInitializer {
 
     public static final String MOD_ID = "lcsl";
 
-    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.create(
-            new Identifier("lcsl", "general"))
-            .icon(() -> new ItemStack(Items.SEA_LANTERN))
-            .appendItems(stacks -> {
-                stacks.add(new ItemStack(ModBlocks.WHITE_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.ORANGE_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.MAGENTA_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.LIGHT_BLUE_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.YELLOW_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.LIME_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.PINK_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.GRAY_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.LIGHT_GRAY_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.CYAN_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.PURPLE_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.BLUE_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.BROWN_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.GREEN_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.RED_SEA_LANTERN));
-                stacks.add(new ItemStack(ModBlocks.BLACK_SEA_LANTERN));
-            })
-            .build();
+    public static final RegistryKey<ItemGroup> ITEM_GROUP_KEY =
+            RegistryKey.of(
+                    Registries.ITEM_GROUP.getKey(),
+                    Identifier.of(MOD_ID, "general")
+            );
 
     @Override
     public void onInitialize() {
-        ModItems.registerItems();
+
+        Registry.register(
+                Registries.ITEM_GROUP,
+                ITEM_GROUP_KEY,
+                FabricItemGroup.builder()
+                        .icon(() -> new ItemStack(Items.SEA_LANTERN))
+                        .displayName(Text.translatable("itemGroup.lcsl"))
+                        .build()
+        );
+
         ModBlocks.registerBlocks();
+        ModItems.registerItems();
+
+        ItemGroupEvents.modifyEntriesEvent(ITEM_GROUP_KEY).register(entries -> {
+            entries.add(ModItems.WHITE_SEA_LANTERN);
+            entries.add(ModItems.ORANGE_SEA_LANTERN);
+            entries.add(ModItems.MAGENTA_SEA_LANTERN);
+            entries.add(ModItems.LIGHT_BLUE_SEA_LANTERN);
+            entries.add(ModItems.YELLOW_SEA_LANTERN);
+            entries.add(ModItems.LIME_SEA_LANTERN);
+            entries.add(ModItems.PINK_SEA_LANTERN);
+            entries.add(ModItems.GRAY_SEA_LANTERN);
+            entries.add(ModItems.LIGHT_GRAY_SEA_LANTERN);
+            entries.add(ModItems.CYAN_SEA_LANTERN);
+            entries.add(ModItems.PURPLE_SEA_LANTERN);
+            entries.add(ModItems.BLUE_SEA_LANTERN);
+            entries.add(ModItems.BROWN_SEA_LANTERN);
+            entries.add(ModItems.GREEN_SEA_LANTERN);
+            entries.add(ModItems.RED_SEA_LANTERN);
+            entries.add(ModItems.BLACK_SEA_LANTERN);
+        });
     }
 }
--- src/main/java/com/yhord/lightly/colored/sea/lanterns/ModBlocks.java
+++ src/main/java/com/yhord/lightly/colored/sea/lanterns/ModBlocks.java
@@ -2,46 +2,64 @@ package com.yhord.lightly.colored.sea.lanterns;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Blocks;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
-    public static final Block WHITE_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block ORANGE_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block MAGENTA_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block LIGHT_BLUE_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block YELLOW_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block LIME_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block PINK_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block GRAY_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block LIGHT_GRAY_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block CYAN_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block PURPLE_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block BLUE_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block BROWN_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block GREEN_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block RED_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
-    public static final Block BLACK_SEA_LANTERN = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
+    private static Block createLantern(MapColor color) {
+        return new Block(FabricBlockSettings.copyOf(Blocks.SEA_LANTERN)
+                .mapColor(color)
+                .luminance(15)
+                .sounds(BlockSoundGroup.GLASS));
+    }
+
+    public static final Block WHITE_SEA_LANTERN = createLantern(MapColor.WHITE);
+    public static final Block ORANGE_SEA_LANTERN = createLantern(MapColor.ORANGE);
+    public static final Block MAGENTA_SEA_LANTERN = createLantern(MapColor.MAGENTA);
+    public static final Block LIGHT_BLUE_SEA_LANTERN = createLantern(MapColor.LIGHT_BLUE);
+    public static final Block YELLOW_SEA_LANTERN = createLantern(MapColor.YELLOW);
+    public static final Block LIME_SEA_LANTERN = createLantern(MapColor.LIME);
+    public static final Block PINK_SEA_LANTERN = createLantern(MapColor.PINK);
+    public static final Block GRAY_SEA_LANTERN = createLantern(MapColor.GRAY);
+    public static final Block LIGHT_GRAY_SEA_LANTERN = createLantern(MapColor.LIGHT_GRAY);
+    public static final Block CYAN_SEA_LANTERN = createLantern(MapColor.CYAN);
+    public static final Block PURPLE_SEA_LANTERN = createLantern(MapColor.PURPLE);
+    public static final Block BLUE_SEA_LANTERN = createLantern(MapColor.BLUE);
+    public static final Block BROWN_SEA_LANTERN = createLantern(MapColor.BROWN);
+    public static final Block GREEN_SEA_LANTERN = createLantern(MapColor.GREEN);
+    public static final Block RED_SEA_LANTERN = createLantern(MapColor.RED);
+    public static final Block BLACK_SEA_LANTERN = createLantern(MapColor.BLACK);
 
     public static void registerBlocks() {
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "white_sea_lantern"), WHITE_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "orange_sea_lantern"), ORANGE_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "magenta_sea_lantern"), MAGENTA_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "light_blue_sea_lantern"), LIGHT_BLUE_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "yellow_sea_lantern"), YELLOW_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "lime_sea_lantern"), LIME_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "pink_sea_lantern"), PINK_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "gray_sea_lantern"), GRAY_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "light_gray_sea_lantern"), LIGHT_GRAY_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "cyan_sea_lantern"), CYAN_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "purple_sea_lantern"), PURPLE_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "blue_sea_lantern"), BLUE_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "brown_sea_lantern"), BROWN_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "green_sea_lantern"), GREEN_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "red_sea_lantern"), RED_SEA_LANTERN);
-        Registry.register(Registry.BLOCK, new Identifier(LightlyColoredSeaLanterns.MOD_ID, "black_sea_lantern"), BLACK_SEA_LANTERN);
+        register("white_sea_lantern", WHITE_SEA_LANTERN);
+        register("orange_sea_lantern", ORANGE_SEA_LANTERN);
+        register("magenta_sea_lantern", MAGENTA_SEA_LANTERN);
+        register("light_blue_sea_lantern", LIGHT_BLUE_SEA_LANTERN);
+        register("yellow_sea_lantern", YELLOW_SEA_LANTERN);
+        register("lime_sea_lantern", LIME_SEA_LANTERN);
+        register("pink_sea_lantern", PINK_SEA_LANTERN);
+        register("gray_sea_lantern", GRAY_SEA_LANTERN);
+        register("light_gray_sea_lantern", LIGHT_GRAY_SEA_LANTERN);
+        register("cyan_sea_lantern", CYAN_SEA_LANTERN);
+        register("purple_sea_lantern", PURPLE_SEA_LANTERN);
+        register("blue_sea_lantern", BLUE_SEA_LANTERN);
+        register("brown_sea_lantern", BROWN_SEA_LANTERN);
+        register("green_sea_lantern", GREEN_SEA_LANTERN);
+        register("red_sea_lantern", RED_SEA_LANTERN);
+        register("black_sea_lantern", BLACK_SEA_LANTERN);
     }
+
+    private static void register(String name, Block block) {
+        Registry.register(
+                Registries.BLOCK,
+                Identifier.of(LightlyColoredSeaLanterns.MOD_ID, name),
+                block
+        );
+    }
+
 }
